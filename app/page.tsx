3f7ebbe797c2cd9ed29610d6793f228d996a3c17
Qassy123 <Q.shah@wlv.ac.uk>
2026-06-01 10:03:15 +0100

    Render urgent delivery CTA on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -21,6 +21,7 @@ export default function Home() {
       <WhyChooseUs />
       <LogisticsProvider />
       <Industries />
+      <UrgentDeliveryCTA />
       <FAQ />
       <Promise />
       <CTA />
